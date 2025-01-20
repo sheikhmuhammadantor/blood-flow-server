d9a5637d37365aa985ac72f544cbfb1670e5465c
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-01-20 10:42:11 +0600

    Refactor authentication middleware and update user registration logic

--- index.js
+++ index.js
@@ -31,20 +31,7 @@ const cookieOptions = {
   sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
 }
 
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
-  if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
-    }
-    req.user = decoded
-    next()
-  })
-}
+// middlewares 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nw7sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -56,10 +43,10 @@ const client = new MongoClient(uri, {
 })
 
 async function run() {
-  try {  
+  try {
 
     // await client.connect();
-    // DB
+    const userCollection = client.db("BloodFlow").collection("Users");
 
     // Generate jwt token
     app.post('/jwt', async (req, res) => {
@@ -71,6 +58,34 @@ async function run() {
         .cookie('token', token, cookieOptions)
         .send({ success: true })
     })
+
+    // // use verify token/user after verifyToken
+    // const verifyToken = (req, res, next) => {
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: 'unauthorized access' });
+    //   }
+    //   const token = req.headers.authorization.split(' ')[1];
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(401).send({ message: 'unauthorized access' })
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   })
+    // }
+
+    // // use verify admin after verifyToken
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await userCollection.findOne(query);
+    //   const isAdmin = user?.role === 'admin';
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: 'forbidden access' });
+    //   }
+    //   next();
+    // }
+
     // Logout
     app.get('/logout', async (req, res) => {
       try {
@@ -82,7 +97,17 @@ async function run() {
       }
     })
 
-// My Apis...
+    // My Apis...
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne({ ...user, role: 'donor' });
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
